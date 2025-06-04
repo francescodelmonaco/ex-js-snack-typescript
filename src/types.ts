bee692039344fac9dd662885b0eda3906a81c4ca
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-06-04 13:44:18 +0200

    add: snack 2 + bonus

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+// 2
+export type Dipendente = {
+    nome: string,
+    cognome: string,
+    annoNascita: number,
+    sesso: "m" | "f",
+    anniDiServizio: number[],
+    readonly emailAziendale: string,
+    contratto: "indeterminato" | "determinato" | "freelance"
+};
